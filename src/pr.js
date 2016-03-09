47de51f6aa8f36d8524e4bf02741abd49a6df282
Oopsno <yufeiliu94@gmail.com>
2016-03-10 01:36:19 +0800

    数据库文件换用绝对路径，添加count方法

--- src/pr.js
+++ src/pr.js
@@ -1,12 +1,17 @@
 var loki = require('lokijs');
 var grunt = require('grunt');
-const localJson = 'symbols.json';
+var path = require('path');
+const localJson = path.resolve(__dirname, '../symbols.json');
 const collectionSymbols = 'symbols';
 
 exports.immutable = function (callback) {
   var db = new loki(localJson);
   db.loadDatabase({}, function (err, data) {
-    callback(null, db.getCollection(collectionSymbols));
+    if (err !== null) {
+      grunt.log.error(err, data);
+    } else {
+      callback(null, db.getCollection(collectionSymbols));
+    }
   });
 };
 
@@ -17,6 +22,16 @@ exports.initialize = function (callback) {
     db.saveDatabase(callback);
   };
   return callback(null, symbols, sync);
+};
+
+exports.count = function (collection) {
+  return collection.mapReduce(function () {
+    return 1;
+  }, function (array) {
+    return array.reduce(function (p, c) {
+      return p + c;
+    }, 0);
+  });
 };
 
 exports.filename = localJson;
